f8724fcaad50380c047bfe3d6bc64a7c37664af9
tolkoxa <tolkoxa@ya.ru>
2021-01-23 20:30:05 +0300

    Тестовое задание №5

--- websites/04/js/todo.js
+++ websites/04/js/todo.js
@@ -1,3 +1,34 @@
-window.onload = function() {
-    document.getElementById('name').focus();
+class Todo {
+    constructor() {
+        this.allData; //массив из json–файла
+        this.task = document.getElementById('task'); //разметка, куда рендерится список дел.
+        this._init();
+    }
+    _init() {
+        this.getList();
+    }
+
+    async getList() {
+        let response = await fetch('data/todo.json');
+        this.allData = await response.json();
+
+        // delete this.allData[0];
+        this.render();
+    }
+
+    render() {
+        this.task.innerHTML = '';
+        this.task.insertAdjacentHTML('beforeend', `
+        <div class="task__item" id="1">
+                    <div class="task__numb" id="numb">1</div>
+                    <input class="task__name" id="name" type="text" placeholder="">
+                    <div class="task__del" id="del">&times;</div>
+                </div>
+        `);
+        document.getElementById('name').focus();
+        console.log(this.allData)
+    }
+
 }
+
+let MyWork = new Todo();

2f06f101aa00a84b7cd946e06928e3e47eb26f43
tolkoxa <tolkoxa@ya.ru>
2021-02-28 22:32:43 +0300

    Исправил ошибку с выводом даты распродажи

--- websites/09/js/shnaider.js
+++ websites/09/js/shnaider.js
@@ -1,14 +1,14 @@
-let dates = document.querySelectorAll('.news__subtitle');
-
-let months = ['января', 'февраля', 'марта', 'апреля', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'],
-    date = new Date(),
+let dates = document.getElementById('sale');
+let date = new Date(),
     searchDay = 1, //0–вс, 1–пн, 2–вт, 3–ср, 4–пт, 6–сб
     secondDate = new Date(),
     delta = searchDay - date.getDay();
-if (delta >= 0) { secondDate.setDate(date.getDate() + delta) } else { secondDate.setDate(date.getDate() + 7 + delta) }
-let str = `${secondDate.getDate()} ${months[secondDate.getMonth()]} ${secondDate.getFullYear()}`;
+if (delta >= 0) { secondDate.setDate(date.getDate() + delta) } else { secondDate.setDate(date.getDate() + 7 + delta) };
 
-dates.forEach((e) => {
-    e.innerHTML = '';
-    e.insertAdjacentHTML('beforeend', str);
-})
+let day = (secondDate.getDate() < 10) ? `0${secondDate.getDate()}` : secondDate.getDate();
+let month = ((secondDate.getMonth() + 1) < 10) ? `0${secondDate.getMonth()+1}` : secondDate.getMonth() + 1;
+
+let str = `${day}.${month}.${secondDate.getFullYear()-2000}`;
+
+dates.innerHTML = '';
+dates.insertAdjacentHTML('beforeend', str);
